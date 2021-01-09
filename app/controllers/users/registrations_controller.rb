f9a62e8165027a59f3bc008bd1ea3ab1bc51fcd7
hrytsiuk <grytsykvika@gmail.com>
2021-01-09 15:23:38 +0200

    code style

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,14 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :select_plan, only: :new
-  
+
   # Extend default Devise gem behavior so that
   # users signing up with the Pro account (plan ID 2)
   # save with a special Stripe subscription function.
   # Otherwise Devise signs up user as usual.
   def create
-    
-   
-    
     super do |resource|
       if params[:plan]
         resource.plan_id = params[:plan]
@@ -22,6 +19,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   private
+
   def select_plan
     unless (params[:plan] == '1' || params[:plan] == '2')
       flash[:notice] = "Please select a membership plan to sign up."

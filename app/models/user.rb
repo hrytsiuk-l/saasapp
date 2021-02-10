21632e774a5c2cce4b36b40488fa096844a6bb06
Liudmyla <hrytsiuk.liudmyla@gmail.com>
2021-02-10 22:16:49 +0200

    Added bare bones user show page and updated profiles redirect

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   belongs_to :plan
-  has_one :profile
+  has_one :profile, :dependent => :destroy
 
   attr_accessor :stripe_card_token
   # If Pro user passes validations (email, password, etc,),
